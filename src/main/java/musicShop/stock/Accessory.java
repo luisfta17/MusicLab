58ee9b3b8c0366136f34f11c1e73520253b572cb
Luis Tejero <luisfaridta@gmail.com>
2018-05-25 11:30:28 +0100

    Instrument and Accesory super classes set up

--- src/main/java/musicShop/stock/Accessory.java
+++ src/main/java/musicShop/stock/Accessory.java
@@ -1,4 +1,28 @@
 package musicShop.stock;
 
-public class Accessory {
+public abstract class Accessory implements ISell {
+    private String type;
+    private int buyingPrice, sellingPrice;
+
+    public Accessory(String type, int buyingPrice, int sellingPrice){
+        this.type = type;
+        this.buyingPrice = buyingPrice;
+        this.sellingPrice = sellingPrice;
+    }
+    
+    public String getType() {
+        return this.type;
+    }
+
+    public int getBuyingPrice() {
+        return this.buyingPrice;
+    }
+
+    public int getSellingPrice() {
+        return this.sellingPrice;
+    }
+    @Override
+    public int calculateMarkup() {
+        return (this.sellingPrice - this.buyingPrice);
+    }
 }
